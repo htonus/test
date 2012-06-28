4ff3d4d54c4a9d5c9fc4c9a3dcd673d5d3a13ca8
Mikhail Cherviakov <m.cherviakov@fxpro.com>
2012-06-28 11:05:58 +0300

    auto-generation

---
+++ src/classes/Business/Application.class.php
@@ -0,0 +1,18 @@
+<?php
+/******************************************************************************
+ *   Copyright (C) 2012 by Mikhail Cherviakov                                 *
+ *   email: htonus@gmail.com                                                  *
+ ******************************************************************************/
+/* $Id$ */
+
+/**
+ * Description of Application
+ *
+ * @author htonus
+ */
+final class Application
+{
+	
+}
+
+?>
